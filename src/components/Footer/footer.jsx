bb6a4d06b2d1176a15c7f332bb1c6f5f2181ba3a
HThanh-how <thanhlt1235@gmail.com>
2023-03-11 19:02:52 +0700

    update inline

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -9,7 +9,7 @@ const Footer = (props) => {
     <footer className={styles.footer}>
        
         <h2 className={styles.name}> About the shop</h2>
-        < SlSocialFacebook size={40}/> <SlSocialInstagram size={40}/> <SlSocialInstagram size={40}/>
+        < SlSocialFacebook size={40} className={styles.icon}/> <SlSocialInstagram size={40} className={styles.icon}/> <SlSocialInstagram size={40} className={styles.icon}/>
         <p className={styles.item}>Privace Policy </p>
         <p className={styles.item}>Return Policy </p>
         <p className={styles.item}>Term of Service </p>
