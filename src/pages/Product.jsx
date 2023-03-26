3c7f67a29031f1ab6182c3a0f124191b022c7b23
lonelyknight2902 <truongthinh2902@gmail.com>
2023-03-26 22:11:11 +0700

    Update: Add product info

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,19 +1,35 @@
-import React from 'react'
-import { ImageSlider } from '../components'
+import React from "react";
+import { ImageSlider, ProductInfo, QuantityButton } from "../components";
 
-const dummy = [
-    "https://product.hstatic.net/1000392212/product/city_map_d942a93abd944547a24949398d9b0deb_master.png",
-    "https://product.hstatic.net/1000392212/product/ca_2_0a509a4471e04e019d8be63c6cba0af6_master.jpg",
-    "https://product.hstatic.net/1000392212/product/sg_9fff9a3cac62490c922ea48226e18aa1_master.jpg",
-    "https://product.hstatic.net/1000392212/product/sg_3_da2bd324295f40b98ca651d3c11fe8fa_master.jpg",
-]
+const dummyImages = [
+  "https://product.hstatic.net/1000392212/product/city_map_d942a93abd944547a24949398d9b0deb_master.png",
+  "https://product.hstatic.net/1000392212/product/ca_2_0a509a4471e04e019d8be63c6cba0af6_master.jpg",
+  "https://product.hstatic.net/1000392212/product/sg_9fff9a3cac62490c922ea48226e18aa1_master.jpg",
+  "https://product.hstatic.net/1000392212/product/sg_3_da2bd324295f40b98ca651d3c11fe8fa_master.jpg",
+];
+
+const dummy = {
+  name: "Saigon Wooden City Map",
+  price: 1250000,
+  description: [
+    "Premium Wooden City Map is marketed as a handcrafted item, yet it nevertheless satisfies all requirements for luxury and natural beauty.",
+    "The product is used to decorate the space, you can hang or set up the product at appropriate, conspicuous positions. Wooden City Map is both a Home Decor and a knowledge of famous cities.",
+    "Material: wood",
+    "Dimensions: 442x320x30mm",
+    "Weight: 800 grams",
+  ],
+  shippingInfo: ["Shipping from Ho Chi Minh City, Vietnam"],
+};
 
 function Product() {
   return (
     <div>
-        <ImageSlider images={dummy}></ImageSlider>
+      <ImageSlider images={dummyImages}></ImageSlider>
+      <div>
+        <ProductInfo {...dummy} />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Product
+export default Product;
