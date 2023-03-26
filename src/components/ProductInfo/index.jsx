50f08e09b2991777b9443b42c396b43e152e2c72
lonelyknight2902 <truongthinh2902@gmail.com>
2023-03-26 22:09:04 +0700

    Update: Add quantity button and styling

--- src/components/ProductInfo/index.jsx
+++ src/components/ProductInfo/index.jsx
@@ -1,25 +1,33 @@
-import React from 'react'
-import styles from './ProductInfo.module.css'
+import React from "react";
+import QuantityButton from "../QuantityButton";
+import styles from "./ProductInfo.module.css";
 
-function ProductInfo({name, price, description, shippingInfo}) {
+function ProductInfo({ name, price, description, shippingInfo }) {
   return (
-    <div>
-        <div className={styles.title}>
-            <h1>{name}</h1>
-            <div>{price}</div>
+    <div className={styles.product}>
+      <div className={styles.title}>
+        <h1>{name}</h1>
+        <div>{price}</div>
+      </div>
+      <div className={styles.info}>
+        <h2>Product description</h2>
+        <ul>
+          {description.map((desc) => (
+            <li>{desc}</li>
+          ))}
+        </ul>
+        <h2>Shipping information</h2>
+        <ul>
+          {shippingInfo.map((ship) => (
+            <li>{ship}</li>
+          ))}
+        </ul>
+        <div className={styles.quantity}>
+          <QuantityButton/>
         </div>
-        <div>
-            <h2>Product description</h2>
-            <ul>
-                {description.map((desc) => <li>{desc}</li>)}
-            </ul>
-            <h2>Shipping information</h2>
-            <ul>
-                {shippingInfo.map((ship) => <li>{ship}</li>)}
-            </ul>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ProductInfo
+export default ProductInfo;
