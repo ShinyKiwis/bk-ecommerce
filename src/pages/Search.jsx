342ad8fddf3fdb8f784a36bd2bac0e39781c62d8
lonelyknight2902 <truongthinh2902@gmail.com>
2023-04-01 16:30:34 +0700

    Update: Implement Search page

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,36 +1,42 @@
 import React from 'react'
 import { ProductItem } from '../components'
+import SearchStyle from '../styles/Search.module.css'
 
 const dummy = [
     {
         name: 'hoho',
-        price: 5000000
+        price: 5000000,
+        image: "https://product.hstatic.net/1000392212/product/city_map_d942a93abd944547a24949398d9b0deb_master.png"
     },
     {
         name: 'hoho',
-        price: 5000000
+        price: 5000000,
+        image: "https://product.hstatic.net/1000392212/product/city_map_d942a93abd944547a24949398d9b0deb_master.png"
     },
     {
         name: 'hoho',
-        price: 5000000
+        price: 5000000,
+        image: "https://product.hstatic.net/1000392212/product/city_map_d942a93abd944547a24949398d9b0deb_master.png"
     },
     {
         name: 'hoho',
-        price: 5000000
+        price: 5000000,
+        image: "https://product.hstatic.net/1000392212/product/city_map_d942a93abd944547a24949398d9b0deb_master.png"
     },
     {
         name: 'hoho',
-        price: 5000000
+        price: 5000000,
+        image: "https://product.hstatic.net/1000392212/product/city_map_d942a93abd944547a24949398d9b0deb_master.png"
     },
 ]
 
 function Search() {
   return (
-    <div>
+    <div className={SearchStyle.content_display}>
         <div>Result for huhu</div>
         <div>this is a filter bar</div>
-        <div>
-            {dummy.map(item => <ProductItem productName={item.name} productPrice={item.price} />)}
+        <div className={SearchStyle.list_display}>
+            {dummy.map(item => <ProductItem productName={item.name} productPrice={item.price} productImage={item.image}/>)}
         </div>
     </div>
   )
